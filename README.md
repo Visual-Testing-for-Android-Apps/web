780cdee0d7e0e22c103765957eb62eae9e9d438a
Damien Smith <damien.smith@monash.edu>
2021-03-31 19:15:43 +1100

    Setup Dependencies & Initial Project Structure

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# web
+## Visual Testing for Android Apps - Web Frontend
+
+## Setup Instructions
+Install the [latest stable](https://nodejs.org/en/) version of NodeJS  
+
+Clone the repository  
+
+In the root directory for the project, run the following  
+```
+npm install
+```
+
+To start the development server, run the following
+```
+npm start
+```
+
+Then navigate to `localhost:3000` in a browser  
+Hot reloading is enabled, so changing a JSX file and saving should immediately be reflected without needing to refresh the page
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/preset-react"] }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+    filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist/"),
+    port: 3000,
+    publicPath: "http://localhost:3000/dist/",
+    hotOnly: true
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin(),
+            new HtmlWebpackPlugin({template: path.join(__dirname, "src", "public", "index.html")})]
+};
