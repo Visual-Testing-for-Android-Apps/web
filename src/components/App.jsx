78c3a72f419fbbb42bba01211d9c07ec0a391417
crystaltr <33253364+crystaltr@users.noreply.github.com>
2021-04-05 14:55:54 +1000

    Added 'Service' page
    current error is duplicating content from homepage to service page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,21 @@
 import React from "react";
 import { hot } from "react-hot-loader";
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Service from './Service';
 
-const App = () => <h1>Hello World</h1>;
+function App(){
+    return(
+        <Router>
+            <div className="App">
+                <h1>Hello World</h1></div>
+            <Switch>
+                <Route path= "/service">
+                    <Service />
+                </Route>
+            </Switch>
+    </Router>
+    );
+}
+
 
 export default hot(module)(App);
---
+++ src/components/Service.js
@@ -0,0 +1,10 @@
+import React from "react";
+const Service = () => {
+    return (
+        <div className="service">
+            <h2>"The Visual Testing platform"</h2>
+        </div>
+    );
+}
+
+export default Service;
