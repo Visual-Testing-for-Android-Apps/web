a153a0d92d4ba815754a78f879a23db4dd4c5460
Gurusewak13 <81728027+Gurusewak13@users.noreply.github.com>
2021-04-14 12:18:53 +1000

    simple File Uploader
    + Added style.css to define size of image to be uploaded
    +FileUpload.jsx which implements file upload functionality
    
    NOTE:
    
    ++ Currently works with images only ++

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
 import React from "react";
+require("./style.css");
 import { hot } from "react-hot-loader";
 import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 import Service from './Service';
 import UploadPage from './UploadPage';
-
+import FileUpload from './FileUpload'
 function App(){
     return(
         <Router>
             <Switch>
                 <Route exact path="/">
-                    <UploadPage />
+
+                    <FileUpload />
                 </Route>
                 <Route path= "/service">
                     <Service />
