6a3acb131541e717b9fc22d362a17c818e759f06
Damien Smith <damien.smith@monash.edu>
2021-04-24 18:48:59 +1000

    Add File Upload Box + Style Form

--- src/components/UploadPage.jsx
+++ src/components/UploadPage.jsx
@@ -1,13 +1,55 @@
-import React from  "react";
+import React, { useState } from "react";
 
-import InputBox from './InputBox';
+import UploadBox from "./UploadBox";
 
 const UploadPage = () => {
-    return (
-        <div>
-            <InputBox />
-        </div>
-    )
-}
+  const [email, setEmail] = useState("");
+  const [files, setFiles] = useState([]);
+
+  const handleChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Upload to server
+  };
+
+  return (
+    <div style={containerStyle}>
+      <form style={formStyle} onSubmit={handleSubmit}>
+        <input
+          style={inputItemStyle}
+          type="email"
+          value={email}
+          onChange={handleChange}
+          placeholder={"Email"}
+        />
+        <UploadBox setFiles={setFiles} />
+        <div>{files.map((f) => f.path)}</div>
+        <input type="submit" value="Submit" style={inputItemStyle} />
+      </form>
+    </div>
+  );
+};
+
+const containerStyle = {
+  display: "flex",
+  justifyContent: "center",
+};
+
+const formStyle = {
+  margin: "2rem 1rem",
+  width: "clamp(5rem, 30rem, 50rem)",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+};
+
+const inputItemStyle = {
+  marginTop: "1rem",
+  height: "2rem",
+  fontSize: "1rem",
+};
 
 export default UploadPage;
