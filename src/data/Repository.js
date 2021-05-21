e2f69e7ce769a966eb096db3a5fda283026c5a82
Damien Smith <damien_g_s@pm.me>
2021-05-21 20:34:24 +1000

    Fix Async/Await Runtime Errors

--- src/data/Repository.js
+++ src/data/Repository.js
@@ -6,11 +6,25 @@ import routes from './API';
  class Repository {
 
     uploadFiles(files) {
-        const formData = new FormData();
-        files.forEach(file => {
-            formData.append("userFile", file);
+        return this._encodeImage(files[0])
+        .then(encodedImage => fetch('https://8uxam9kkod.execute-api.ap-southeast-2.amazonaws.com/Prod/owleye/', { method: "POST", body: encodedImage.split(',')[1] }))
+        .then(response => response.json())
+        .then(json => this._decodeImage(json["res_img"]))
+    }
+
+    _encodeImage(image) {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.onloadend = () => resolve(reader.result);
+            reader.onerror = reject;
+            reader.readAsDataURL(image);
         })
-        return fetch(routes.upload, { method: "POST", body: formData });
+    }
+
+    _decodeImage(imageAsString) {
+        const image = new Image();
+        image.src = `data:image/png;base64,${imageAsString}`;
+        return image;
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom";
+// Necessary import to support async/await
+import 'regenerator-runtime/runtime';
 
 import App from "./components/App";
 import './index.css'
