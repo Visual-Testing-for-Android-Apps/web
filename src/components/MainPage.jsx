5a49ea64ffce9c93fc024bd5080ee80a5dc5f603
Gurusewak13 <81728027+Gurusewak13@users.noreply.github.com>
2021-05-25 22:48:31 +1000

    Added About section on main page
    + about page css file
    + about section jsx file
    + image icon for about section

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -3,12 +3,14 @@ import React from "react";
 import UploadSection from "./UploadPage";
 import "./mainpage.css";
 import ExampleSection from "./ExampleSection";
+import AboutSection from "./About-Section";
 
 const MainPage = () => {
     return (
         <div className="section__container">
             <UploadSection />
             <ExampleSection />
+            <AboutSection />
         </div>
     )
 }
