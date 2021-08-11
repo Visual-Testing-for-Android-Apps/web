f58a53a6af25d61a342f7ff2111738c7f7c52173
leanne-b <81729265+leanne-b@users.noreply.github.com>
2021-08-11 18:35:26 +1000

    job selection css changes

--- src/components/mainPage/JobSelectionSection.jsx
+++ src/components/mainPage/JobSelectionSection.jsx
@@ -1,16 +1,15 @@
 import React, { forwardRef } from "react";
 import "./mainpage.css";
-import BatchIcon from "../images/batchIcon.png";
+import BatchIcon from "../images/batchIcon2.png";
 import LiveIcon from "../images/liveJobIcon.png";
 
 const JobSelectionSection = forwardRef((props, ref) => {
   function renderButton(icon, jobType, description) {
     return (
       <button className="jobBtn">
-        {" "}
-        <img src={icon} />
-        <h1>{jobType}</h1>
-        <p>{description}</p>
+        <img className="img-size" src={icon} />
+        <h1 className="jobType">{jobType}</h1>
+        <p className="jobDescription">{description}</p>
       </button>
     );
   }
@@ -20,23 +19,15 @@ const JobSelectionSection = forwardRef((props, ref) => {
       <div className="selection-heading">
         <h1 className="title">What do you want to do?</h1>
       </div>
-      <div>
-        {renderButton(LiveIcon, "Live Job", "Description 1")}
-        {renderButton(BatchIcon, "Batch Icon", "Something")}
+      <div className="btnContainer">
+        {renderButton(LiveIcon, "LIVE JOB", "See results in real time. Up to 10 images and videos")}
+        <span className="btn-space"></span>
+        {renderButton(
+          BatchIcon,
+          "BATCH JOB",
+          "Submit up to 100 images and videos. Get an email report"
+        )}
       </div>
-
-      {/* <div className="btnContainer">
-                <button className="liveBtn">
-                    
-                    <h1>Live Job</h1>
-                    <p>explanation</p>
-                </button>
-                
-                <button className="batchBtn">
-                    <h1>Batch Job</h1>
-                    <p>somethign</p>
-                </button> */}
-      {/* </div> */}
     </div>
   );
 });
