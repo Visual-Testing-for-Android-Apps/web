6569e3ab2ae9029070ed28d668217750127f0001
leanne-b <81729265+leanne-b@users.noreply.github.com>
2021-08-20 15:35:56 +1000

    removing reportpage from app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import MainPage from "./mainPage/MainPage";
-import ReportPage from "./reportPage/ReportPage";
 import LiveJob from "./reportPage/LiveJob";
 import BatchJob from "./reportPage/BatchJob";
 import Navigation from "./navbar/Navigation";
@@ -14,13 +13,9 @@ function App() {
         <Route exact path="/">
           <MainPage />
         </Route>
-        <Route path="/reportpage">
-          <ReportPage />
-        </Route>
         <Route path="/livejob">
           <LiveJob />
         </Route>
-
         <Route path="/batchjob">
           <BatchJob />
         </Route>
