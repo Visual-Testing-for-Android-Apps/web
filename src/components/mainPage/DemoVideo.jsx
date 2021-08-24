5fd56b94241a4eed19b535361b616c22641af402
leanne-b <81729265+leanne-b@users.noreply.github.com>
2021-08-24 21:33:18 +1000

    ui changes for mobile main page

--- src/components/mainPage/DemoVideo.jsx
+++ src/components/mainPage/DemoVideo.jsx
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 const DemoVideo = ({ embedId }) => (
   <div className="video-responsive">
     <iframe
+      className="demo-vid"
       width="500"
       height="280"
       src={`https://www.youtube.com/embed/${embedId}`}
