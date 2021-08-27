516b5357bea15384180026641ce229f0a8a89c37
akshithP <apat0017@student.monash.edu>
2021-08-27 20:36:25 +1000

    Option to change email after submitting

--- src/components/reportPage/BatchJob.jsx
+++ src/components/reportPage/BatchJob.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import UploadSection from "./UploadSection";
 import "./batch-job.css";
 
@@ -7,6 +7,8 @@ const BatchJob = () => {
   const [files, setFiles] = useState([]);
   const [email, setEmail] = useState();
   const [disableSubmit, setDisableSubmit] = useState(true);
+  const [emailVerified, setEmailVerified] = useState(false);
+  const [status, setStatus] = useState("Submit");
 
   const handleEmail = async (e) => {
     const { email } = e.target.elements;
@@ -15,18 +17,40 @@ const BatchJob = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    handleEmail(e);
-    console.log(email)
+    if (status != "Submit") {
+      setStatus("Submit");
+      setEmailVerified(false);
+      setDisableSubmit(true);
+    } else {
+      handleEmail(e);
+      setStatus("Change");
+    }
   };
+
+  useEffect(() => {
+    if (emailVerified) {
+      setStatus("Change");
+      setDisableSubmit(false);
+    } else {
+      setDisableSubmit(true);
+    }
+  }, [emailVerified]);
 
   return (
     <div className="section_container">
       <h1 style={{ textAlign: "center" }}>
         <b>Upload Files</b>
       </h1>
-      <form className="form-container">
-        <input type="email" id="email" placeholder="Enter your email..." />
-        <button onClick={() => handleEmail}>Submit</button>
+      <form className="form-container" onSubmit={handleSubmit}>
+        <input
+          type="email"
+          id="email"
+          placeholder="Enter your email..."
+          disabled={!disableSubmit}
+          required
+          defaultValue={email}
+        />
+        <button type="submit">{status}</button>
       </form>
       <UploadSection fileLimit={FILE_LIMIT} setFiles={setFiles} files={files} />;
     </div>
