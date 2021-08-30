03f3fc6e5be7b642d92af78f58dd2bfbdb16e500
akshithP <apat0017@student.monash.edu>
2021-08-30 18:16:33 +1000

    Removed option to change email in BatchSubmitPage

--- src/components/reportPage/BatchSubmitPage.jsx
+++ src/components/reportPage/BatchSubmitPage.jsx
@@ -5,58 +5,12 @@ import "./batch-job.css";
 
 const BatchSubmitPage = () => {
   const { email } = useLocation().state ?? {};
-  const [newEmail, setNewEmail] = useState();
-  const [emailConfirm, setConfirm] = useState(true);
-  const [disableSubmit, setDisableSubmit] = useState(true);
-  const [status, setStatus] = useState("Change");
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (status != "Confirm") {
-      console.log("ran if statement")
-      setStatus("Confirm");
-      setConfirm(false);
-    } else {
-      handleEmailChange(e);
-      setConfirm(true);
-      setStatus("Change");
-    }
-  };
-
-  useEffect(() => {
-    if (emailConfirm) {
-      setStatus("Change");
-      setDisableSubmit(false);
-    } else {
-      setDisableSubmit(true);
-    }
-  }, [emailConfirm]);
-
-  const emailRef = useRef();
-  emailRef.current = email;
-
-  const handleEmailChange = async (e) => {
-    const { email } = e.target.elements;
-    setNewEmail(email.value);
-    setConfirm(true);
-  };
 
   return (
     <div className="section_container">
       <h1 style={{ textAlign: "center" }}>Batch Job Submitted Successfully!</h1>
       <p style={{ textAlign: "center", fontSize: 18 }}>Your results will be sent to following email address:</p>
-      <form className="email-form-container" onSubmit={handleSubmit}>
-      <input
-          type="email"
-          id="email"
-          placeholder="Enter your email..."
-          required
-          defaultValue={email}
-          disabled={!disableSubmit}
-        />
-        <button type="submit">{status}</button>
-      </form>
-      
+      <p style={{ textAlign: "center", fontSize: 18}}>{email}</p>
     </div>
   );
 };
