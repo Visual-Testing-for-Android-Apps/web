2ea66b9480de215aeca38a6c93ab86600a2dee60
crystaltr <33253364+crystaltr@users.noreply.github.com>
2021-09-03 09:56:19 +1000

    Changed styling of dropdown

--- src/components/reportPage/ReportPage.jsx
+++ src/components/reportPage/ReportPage.jsx
@@ -93,35 +93,33 @@ const ReportPage = (props) => {
             className="file-search"
             value={searchTerm}
             onChange={handleSearching}
-          />
-          <div>
-            <select
+          />{" "}
+          <select
             onChange={(e) => {
-              setFilterType(e.target.value)
-              // filterImages(e.target.value);
+              setFilterType(e.target.value);
             }}
             className="custom-select"
-       aria-label="Filter Countries By Region">
-        <option value="All">Show All Results</option>
-        <option value="Null value">Null value</option>
-        <option value="Component occlusion">Component occlusion</option>
-        <option value="Missing image">Missing image</option>
-        <option value="No defect found">No defect found</option>
-        </select>
-        <span className="focus"></span>
-          </div>
+            aria-label="Filter Countries By Region"
+          >
+            <option value="All">Show All Results</option>
+            <option value="Null value">Null value</option>
+            <option value="Component occlusion">Component occlusion</option>
+            <option value="Missing image">Missing image</option>
+            <option value="No defect found">No defect found</option>
+          </select>
         </div>
+        <div></div>
       </div>
       {imageResults.length > 0 && <h1 className="results-title">Image Results</h1>}
       {imageResults.length > 0 && <ColourSchemeSelector setColourScheme={setColourScheme} />}
       <div className="results">
         <div className="results-container">
           {imageResults.reduce((previousResult, currentResult, index) => {
-            //console.log(currentResult["bug_type"])
-            const bugTypes = currentResult["bug_type"].filter(bugType => bugType === filterType)
-            console.log(bugTypes)
+            const bugTypes = currentResult["bug_type"].filter((bugType) => bugType === filterType);
             if (
-              (filterType === "All" ||  (filterType === "No defect found" && currentResult["bug_type"].length === 0) ||  bugTypes[0] === filterType )
+              filterType === "All" ||
+              (filterType === "No defect found" && currentResult["bug_type"].length === 0) ||
+              bugTypes[0] === filterType
             ) {
               return [
                 ...previousResult,
