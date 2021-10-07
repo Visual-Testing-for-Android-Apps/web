d9b8742a7c0c9b233500abf92c09954f3ef9e09f
leanne-b <81729265+leanne-b@users.noreply.github.com>
2021-10-07 20:22:29 +1100

    clean-up

--- src/components/reportPage/LiveJob.jsx
+++ src/components/reportPage/LiveJob.jsx
@@ -4,7 +4,7 @@ import { useHistory } from "react-router-dom";
 
 const LiveJob = () => {
   const FILE_LIMIT = 10;
-  const MAX_FILE_SIZE = 6;
+  const MAX_FILE_SIZE_MB = 6;
   const history = useHistory();
   const handleJob = (files) => {
     history.push("/reportpage", { files: files });
@@ -15,7 +15,7 @@ const LiveJob = () => {
       <h1 style={{ textAlign: "center" }}>
         <b>Live Job Request</b>
       </h1>
-      <UploadSection fileLimit={FILE_LIMIT} handleJob={handleJob} maxFileSize={MAX_FILE_SIZE} />
+      <UploadSection fileLimit={FILE_LIMIT} handleJob={handleJob} maxFileSize={MAX_FILE_SIZE_MB} />
     </div>
   );
 };
